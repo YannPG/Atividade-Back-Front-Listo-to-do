5a414545012e3ae56993423401cc08a74b847c56
YannPG <yann_pg@hotmail.com>
2025-09-01 07:13:34 -0300

    feat: adiciona componente de rodape com link para o github

--- listadetarefas-web/src/app/app.component.ts
+++ listadetarefas-web/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TaskListComponent],
-  template: `<app-task-list></app-task-list>`,
+  imports: [TaskListComponent, FooterComponent, CommonModule],
+  templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+}
